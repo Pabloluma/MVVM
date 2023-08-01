3740d50407ab94ad5efb4472cf3e1d4a7f80aaf9
Pablo <pablolupionmarquez@gmail.com>
2023-08-01 10:24:33 +0200

    Cambio de color de fondo

--- app/src/main/java/com/pablo/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/pablo/mvvm/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.pablo.mvvm.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.pablo.mvvm.databinding.ActivityMainBinding
 import com.pablo.mvvm.viewModel.ViewModelClass
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         })
         binding.viewContainer.setOnClickListener{
             fraseViewModel.randomFrase()
+            binding.viewContainer.setBackgroundColor(Color.rgb(Random.nextInt(256),Random.nextInt(256),Random.nextInt(256)))
         }
     }
 }
